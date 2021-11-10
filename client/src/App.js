bab5c758682db7fe2e81f86163be3da0b380f351
Matthew Sievers <mattersievers@gmail.com>
2021-11-10 15:22:40 -0600

    routes are working,still need to fix hard coded thoughts and user profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,12 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -19,13 +25,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-      <div className='container'>
-        <Home />
-      </div>
-        <Footer />
-      </div>
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route exact path="/" element={<Home/>} />
+              <Route exact path="/login" element={<Login/>} />
+              <Route exact path="/signup" element={<Signup/>} />
+              <Route exact path="/profile/:username?" element={<Profile/>} />
+              <Route exact path="/thought/:id" element={<SingleThought/>} />
+
+              <Route path="*" element={<NoMatch/>} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>  
     </ApolloProvider>
     
   );
